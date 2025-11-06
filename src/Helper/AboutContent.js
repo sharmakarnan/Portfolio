5d8daaa0bf8b7b0e585169313b522f72a5e506d8
sharmakarnan <sharmakarnan16@gmail.com>
2025-11-06 21:26:33 +0530

    Fixed AboutContent build issue

--- src/Helper/AboutContent.js
+++ src/Helper/AboutContent.js
@@ -5,6 +5,13 @@ import React1 from "../assets/IntoImg.jpg";
 
 
 const AboutContent = () => {
+
+   const handleRedeploy = async () => {
+    await fetch("https://api.vercel.com/v1/integrations/deploy/prj_F7wTKjCPuBsm0veja9dX7Sqn00yM/A8UAZY5bMo", {
+      method: "POST"
+    });
+    alert("✅ Redeploy triggered successfully!");
+  };
   return (
     <div className='about'>
         <div className='left'>
@@ -21,7 +28,7 @@ const AboutContent = () => {
         </div>
         <div className='right'>
             <div className='img-container'>
-                <div className='img-stact top'>
+                <div className='img-stack top'>
                     <img src={React1} className='img' alt='true'/>
                 </div>
             </div>
